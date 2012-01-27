aed19b54e1baf87b3ef7d500bcd8c842c3f7162e
Noah <noah.singer@gmail.com>
2012-01-26 18:08:04 -0600

    New rails 3.2 app generated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Flashcards::Application.config.secret_token = '2f2d860c819fb93e7b06c6f706b83b8ab07c6c0f0dbfe8b386abd89b5afa4bf971f20adbcffb5b764f828800ff353508c55539bf1c34d4e9b68433b105701c9e'

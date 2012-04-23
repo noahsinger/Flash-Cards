d8cd6bd59fdf5144aed9d2e79bf3c790c77cdae9
Noah <noah.singer@gmail.com>
2012-04-23 08:35:09 -0500

    fixed broken test

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,8 +4,9 @@ require 'digest/sha2'
 class UserTest < ActiveSupport::TestCase
   
   test "should allow valid email addresses" do
+    old_count = User.count
     user1 = User.create( :email => "noah@test.com", :password => "secret" )
-    assert_equal 4, User.count
+    assert_equal old_count+1, User.count
   end
   
   test "should have a reg_hash" do
